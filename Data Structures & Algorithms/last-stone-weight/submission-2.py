e69b928967ff1ef5c6f8f75663366b4cc5c5f105
Adarsh <adarsh.bobade26@gmail.com>
2026-07-22 18:30:16 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.py
@@ -0,0 +1,54 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        def delete_max(heap , i):
+            if heap is None :
+                return 0
+            hv = heap[0]
+            last = heap.pop()
+            if heap :
+                heap[0] = last
+                heapify(heap , 0)
+            return hv
+        
+        def insert(arr , val):
+            arr.append(val)
+            i = len(arr) - 1
+            while i > 0 and arr[i] > arr[(i -1 )//2]:
+                arr[i] , arr[(i -1 )//2] = arr[(i -1 )//2] , arr[i]
+                i = (i-1)//2
+
+
+        
+        def heapify(arr , i):
+            size = len(arr)
+            while True:
+                largest = i
+                l , r = (2*i + 1) , (2*i + 2)
+                if l < size and arr[l] > arr[largest] :
+                    largest = l
+                if r < size and arr[r] > arr[largest]:
+                    largest = r
+                if largest == i :
+                    break
+                arr[i] , arr[largest] = arr[largest] , arr[i]
+                i = largest
+
+        def build_heap(arr):
+            for i in range(len(arr)//2 -1 ,-1,-1):
+                heapify(arr , i)
+
+        build_heap(stones)
+        while stones and len(stones) > 1:
+            x = delete_max(stones , 0)
+            y = delete_max(stones , 0)
+            if x < y or y < x :
+                insert(stones , abs(x - y))
+        
+        return stones[0] if stones else 0
+            
+
+
+
+
+
+        
